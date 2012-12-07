60349088585541b267dd1e0132740a87f614dda0
Andy Sturges <arsturges@lbl.gov>
2012-12-07 15:59:56 -0800

    Flask framework + bootstrap

---
+++ feedme.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+app.debug = False # Set to false before deploying!
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html', title = "Home")
+
+@app.route('/help')
+def help():
+    return render_template('help.html', title = "Help") 
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = "About") 
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title = "Contact") 
+
+if __name__ == '__main__':
+    app.run() #disable app.debug before pushing to production.
